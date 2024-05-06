ddebe977cf7739aee6f1ef67a86bb33de6ca2f43
KimGSeok <rla99tjr@gmail.com>
2024-05-06 13:59:31 +0900

    account change

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             By{' '}
             <Image
               src="/vercel.svg"
-              alt="Vercel Logo"
+              alt="Vercel Logos"
               className={styles.vercelLogo}
               width={100}
               height={24}
